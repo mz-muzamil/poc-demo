f685c3157ef19a94e0745dac1168738eff84a820
mzmuzamil <muhammad.muzamil@fore.studio>
2024-08-27 17:13:33 +0500

    small style tweaks

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -44,13 +44,13 @@ const HomeComponent = () => {
             {apiData.map((singleRecord) => (
               <div
                 key={singleRecord.key}
-                className="flex flex-col justify-center bg-white group shadow-[0_4px_25px_rgba(56,75,255,0.15)] transform hover:-translate-y-1 relative overflow-hidden transition delay-150 duration-200 ease-in-out"
+                className="flex flex-col justify-center bg-white group shadow-[0_4px_25px_rgba(56,75,255,0.15)] transform relative overflow-hidden transition delay-150 duration-200 ease-in-out"
               >
                 <div className="absolute top-0 w-full h-1 right-0 bg-slate-900"></div>
                 <div className="w-full h-[250px] bg-slate-900/10 mb-5 flex justify-center group-hover:border-white">
                   <img src={postImg} alt="post banner" />
                 </div>
-                <div className="text p-8">
+                <div className="text p-5 mb-20">
                   <div className="mb-3 mt-3 text-xl font-oswald font-semibold text-slate-900 xl:text-xl">
                     <Link to={`/post/${singleRecord.key}`}>
                       {singleRecord.name}
@@ -59,20 +59,20 @@ const HomeComponent = () => {
                   <div className="flex justify-center flex-col group-hover:text-black transition delay-150 duration-200 ease-in-out">
                     <p>{singleRecord.description}</p>
                   </div>
-                  <div className="flex gap-5 justify-between">
-                    <Link
-                      to={`/post/${singleRecord.key}`}
-                      className="mt-5 flex bg-slate-900 text-white px-2 py-1 rounded justify-center"
-                    >
-                      View Detail
-                    </Link>
-                    <button
-                      onClick={() => handleDelete(singleRecord.key)}
-                      className="mt-5 flex bg-red-600 text-white px-2 py-1 rounded justify-center"
-                    >
-                      Delete
-                    </button>
-                  </div>
+                </div>
+                <div className="flex gap-5 justify-between absolute bottom-0 w-full p-5 right-0">
+                  <Link
+                    to={`/post/${singleRecord.key}`}
+                    className="mt-5 flex bg-slate-900 text-white px-2 py-1 rounded justify-center"
+                  >
+                    View Detail
+                  </Link>
+                  <button
+                    onClick={() => handleDelete(singleRecord.key)}
+                    className="mt-5 flex bg-red-600 text-white px-2 py-1 rounded justify-center"
+                  >
+                    Delete
+                  </button>
                 </div>
               </div>
             ))}
